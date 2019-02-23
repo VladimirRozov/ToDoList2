1f6ef69c22bac1eba5de6e30a1bd337e25509f4c
VladimirRozov <39130781+VladimirRozov@users.noreply.github.com>
2019-02-23 15:51:49 +0300

    Переход между вкладками по кнопкам вкладка раписание пока перенаправяляет на добавление задачи, т.к. не реализование распиание

---
+++ app/src/main/java/com/practice/work/Main2Activity.kt
@@ -0,0 +1,35 @@
+package com.practice.work
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+               // message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                val intent = Intent(this, NewTaskActivity::class.java)
+                startActivityForResult(intent, 0)
+            }
+            R.id.navigation_notifications -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivityForResult(intent, 0)
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
